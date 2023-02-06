de8188c180bd58b71c35c11e6bc572c4783f06dc
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-06 12:55:15 -0300

    setting up prisma

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,1 +1,14 @@
-export class CreateTodoDto {}
+import { IsString } from "class-validator";
+import { IsBoolean, IsNumber } from "class-validator/types/decorator/decorators";
+
+export class CreateTodoDto {
+
+    @IsNumber
+    id: number;
+
+    @IsString
+    description: string;
+
+    @IsBoolean
+    completed: boolean;
+}
