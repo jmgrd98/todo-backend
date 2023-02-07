25b212f19440335b8a3739a8a7a52647ca2b9581
João Marcelo Dantas <jmgrd98@gmail.com>
2023-02-07 10:01:14 -0300

    commit

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -3,9 +3,6 @@ import { IsString, IsBoolean, IsDate } from "class-validator";
 export class CreateTodoDto {
 
     @IsString()
-    id: string;
-
-    @IsString()
     description: string;
 
     @IsBoolean()
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -3,9 +3,6 @@ import { IsString, IsBoolean, IsDate } from "class-validator";
 export class Todo {
 
     @IsString()
-    id: string;
-
-    @IsString()
     description: string;
 
     @IsBoolean()
